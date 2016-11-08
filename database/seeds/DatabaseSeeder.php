13c9214b404a9615425ed0b58e093f3b7a114c19
xzjs <2310976219@qq.com>
2016-11-08 21:04:14 +0800

    更新种子填充器

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -13,5 +13,6 @@ class DatabaseSeeder extends Seeder
     {
         // $this->call(UsersTableSeeder::class);
         $this->call(ItemSeeder::class);
+        $this->call(PointerSeeder::class);
     }
 }
