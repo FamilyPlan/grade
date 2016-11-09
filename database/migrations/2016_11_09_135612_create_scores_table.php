7ca1d71c77fcfa6954ed0580c03c4088d7c7b8f8
xzjs <2310976219@qq.com>
2016-11-09 14:29:29 +0800

    完成打分接口，并更新相关的文档

---
+++ database/migrations/2016_11_09_135612_create_scores_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateScoresTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('scores', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('match_id');
+            $table->integer('car_id');
+            $table->integer('point_id');
+            $table->integer('score');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('scores');
+    }
+}
